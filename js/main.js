ff51ff9aa46d4ba1b92dd1711f6c7bfc7226d0a8
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-10-26 15:07:24 +0200

    ADD: Roll the dice

--- js/main.js
+++ js/main.js
@@ -19,3 +19,29 @@ buttonValidate.addEventListener('click', () => {
         }
     }
 });
+
+/* Roll the Dice */
+const rollBtn = document.getElementById('roll');
+const restartBtn = document.getElementById('restart');
+
+const textUser = document.getElementById('text-user');
+const textPc = document.getElementById('text-pc');
+const textResult = document.getElementById('text-result');
+
+rollBtn.addEventListener('click', () => {
+    const userRoll = Math.floor(Math.random() * 6) + 1;
+    const PcRoll = Math.floor(Math.random() * 6) + 1;
+
+    textUser.innerHTML += ` <span>${userRoll}</span>`;
+    textPc.innerHTML += ` <span>${PcRoll}</span>`;
+
+    if (userRoll > PcRoll) textResult.innerHTML = 'Yeah... You win 🎉';
+    else if (userRoll === PcRoll) textResult.innerHTML = 'Mhh... It is a draw 😐';
+    else textResult.innerHTML = 'Oh no... Ai always wins 🤖';
+});
+
+restartBtn.addEventListener('click', () => {
+    textUser.innerHTML = `You -`;
+    textPc.innerHTML = `AI -`;
+    textResult.innerHTML = `Result -`;
+});
